7d9b89c5b2ea9a9a247632802bcf46b14152ea91
DTLJ-planeblob <dailytouslesjours@gmail.com>
2013-09-25 14:31:23 -0400

    proper renaming to Actuator, new switcher access scripts

--- scripts/switcherBaslerRec.sh
+++ scripts/switcherBaslerRec.sh
@@ -12,6 +12,11 @@ killall switcher
 switcher -d &
 sleep 1
 
+# switcher-ctrl -C uridecodebin video
+# switcher-ctrl -s video loop true
+# switcher-ctrl -i video to_shmdata 'file:///home/radioair/Documents/blobserver/_grabs/video.avi'
+
 switcher-ctrl -C uridecodebin video
 switcher-ctrl -s video loop true
-switcher-ctrl -i video to_shmdata 'file:///home/radioair/Documents/blobserver/_grabs/video.avi'
+switcher-ctrl -s video uri 'file:///home/radioair/Documents/blobserver/_grabs/video.avi'
+switcher-ctrl -s video started true
