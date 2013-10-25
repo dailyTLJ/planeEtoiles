7ca1974031f93ca9fbf5dfac2713f4dd34001e10
DTLJ-planeblob <dailytouslesjours@gmail.com>
2013-10-25 11:01:56 -0400

    new shared-memory naming

--- scripts/run_basler_recording.sh
+++ scripts/run_basler_recording.sh
@@ -11,17 +11,20 @@
 source helpers.sh
 
 # cd ../data
-# blobserver -C basler_rec.xml $@ &
-blobserver -C basler_rec_eva.xml $@ &
+# blobserver -C basler_rec.xml
+blobserver -C basler_rec.xml $@ &
+
 sleep 5
-# shmvid /tmp/blobserver_output_1
+shmvid /tmp/blobserver_1_hog_0 &
+shmvid /tmp/blobserver_2_bgsubtractor_0
 
 #gst-launch-0.10 shmsrc socket-path=/tmp/blobserver_output_2 ! \
 #	gdpdepay ! decodebin2 ! ffmpegcolorspace ! \
 #	fpsdisplaysink sync=false force-aspect-ratio=true &
-gst-launch-0.10 shmsrc socket-path=/tmp/blobserver_output_1 ! \
-	gdpdepay ! decodebin2 ! ffmpegcolorspace ! \
-	fpsdisplaysink sync=false force-aspect-ratio=true 
+# gst-launch-0.10 shmsrc socket-path=/tmp/blobserver_output_1 ! \
+# 	gdpdepay ! decodebin2 ! ffmpegcolorspace ! \
+# 	fpsdisplaysink sync=false force-aspect-ratio=true 
+
 # disp-width=640 disp-height=480
 # shmvid /tmp/blobserver_output_2 &
 # shmvid /tmp/blobserver_output_1
